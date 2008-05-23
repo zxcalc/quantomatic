a99a4bb4f8267bac3e5f0711fc3b6162c4709f09
lucasd <lucasd@f8f375fb-4f10-0410-8d47-b6fbdc7ca053>
2008-05-23 16:12:54 +0000

    heap file now is put ~/.quntomatic/ and still called quanto
    git-svn-id: https://isaplanner.svn.sourceforge.net/svnroot/isaplanner/trunk/quantomatic@1574 f8f375fb-4f10-0410-8d47-b6fbdc7ca053

--- core/makeheap.sh
+++ core/makeheap.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-rm $HOME/isabelle/heaps/Isabelle2007/polyml-5.1_x86-darwin/quanto
-isabelle  -e 'use "ROOT.ML"; SaveState.saveState "quanto" ; quit ();' HOL_IsaP
-mv quanto $HOME/isabelle/heaps/Isabelle2007/polyml-5.1_x86-darwin/
+if [ ! -d $HOME/.quantomatic ]; then echo "Making a new .quantomatic directory!"; mkdir $HOME/.quantomatic; else echo "You already have a .quantomatic dir, hurray!"; fi
+
+if [ -f $HOME/.quantomatic/quanto ]; then echo "Remving old quanto heap file."; rm $HOME/.quantomatic/quanto; fi
+
+echo "Building new quanto heap file in $HOME/.quantomatic/quanto"
+isabelle -e "use \"ROOT.ML\"; SaveState.saveState \"$HOME/.quantomatic/quanto\" ; quit ();" HOL_IsaP
+
+echo "Quantomatic heap built!"
