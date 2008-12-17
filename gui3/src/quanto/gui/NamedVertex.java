b6f3ce94cc7a5665fe1d7fa89f6e230fef57a5b7
akissinger <akissinger@f8f375fb-4f10-0410-8d47-b6fbdc7ca053>
2008-12-17 21:38:30 +0000

    refactoring pt 1
    git-svn-id: https://isaplanner.svn.sourceforge.net/svnroot/isaplanner/trunk/quantomatic@1927 f8f375fb-4f10-0410-8d47-b6fbdc7ca053

--- gui3/src/quanto/gui/NamedVertex.java
+++
@@ -1,9 +0,0 @@
-package quanto.gui;
-
-/*
- * Interface for vertices which (in a given graph) all have unique names.
- */
-public interface NamedVertex {
-	public String getName();
-	public void setName(String name);
-}
--- gui3/src/quanto/gui/QuantoVisualizer.java
+++ gui3/src/quanto/gui/QuantoVisualizer.java
@@ -13,6 +13,7 @@ import org.apache.commons.collections15.Transformer;
 
 import edu.uci.ics.jung.algorithms.layout.*;
 import edu.uci.ics.jung.algorithms.layout.util.Relaxer;
+import edu.uci.ics.jung.contrib.SmoothLayoutDecorator;
 import edu.uci.ics.jung.visualization.VisualizationViewer;
 import edu.uci.ics.jung.visualization.decorators.EdgeShape;
 import edu.uci.ics.jung.visualization.picking.PickedState;
