8a8445e1eadee7efeef9e31f124310837b3c2c16
akissinger <akissinger@f8f375fb-4f10-0410-8d47-b6fbdc7ca053>
2009-06-19 14:05:40 +0000

    added ability to activate and deactivate theories. active state visible from gui
    git-svn-id: https://isaplanner.svn.sourceforge.net/svnroot/isaplanner/trunk/quantomatic@2287 f8f375fb-4f10-0410-8d47-b6fbdc7ca053

--- gui/src/quanto/gui/TheoryTree.java
+++ gui/src/quanto/gui/TheoryTree.java
@@ -1,22 +1,29 @@
 package quanto.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
 
 public class TheoryTree extends JPanel {
 	private static final long serialVersionUID = 9201368442015685164L;
 	private JTree tree;
 	private DefaultMutableTreeNode top;
+	protected Set<String> activeTheories;
 
 	public TheoryTree () {
 		setLayout(new BorderLayout());
 		top = new DefaultMutableTreeNode("Theories");
 		tree = new JTree(top);
-		
+		tree.setCellRenderer(new TheoryCellRenderer());
+		activeTheories = new HashSet<String>();
 		
 		// don't want to steal keyboard focus from the active InteractiveView
 		tree.setFocusable(false);
@@ -28,21 +35,62 @@ public class TheoryTree extends JPanel {
 	
 	public void refresh() {
 		top.removeAllChildren();
-		DefaultMutableTreeNode thy;
+		DefaultMutableTreeNode node;
 		QuantoCore core = QuantoApp.getInstance().getCore();
 		try {
-			for (String thyName : core.list_theories()) {
-				thy = new DefaultMutableTreeNode(thyName);
-				for (String ruleName : core.list_rules(thyName)) {
-					thy.add(new DefaultMutableTreeNode(ruleName));
+			String[] thys = core.list_theories();
+			updateActiveTheories(thys, core.list_active_theories());
+			for (String thy : thys) {
+				node = new DefaultMutableTreeNode(thy);
+				for (String rule : core.list_rules(thy)) {
+					node.add(new DefaultMutableTreeNode(rule));
 				}
-				top.add(thy);
+				top.add(node);
 			}
 		} catch (QuantoCore.ConsoleError err) {
 			// should never get a console error during a refresh()
 			throw new QuantoCore.FatalError(err.getMessage());
 		}
 		tree.expandRow(0);
+	}
+	
+	private void updateActiveTheories(String[] all, String[] active) {
+		activeTheories.clear();
+		for (String s : active) activeTheories.add(s);
+	}
+	
+	@SuppressWarnings("serial")
+	private class TheoryCellRenderer extends DefaultTreeCellRenderer {
+
+		public Component getTreeCellRendererComponent(JTree tree, Object value,
+				boolean selected, boolean expanded, boolean leaf, int row,
+				boolean hasFocus) {
+			// pull a nice icon'ed component from the parent
+			Component cell = super.getTreeCellRendererComponent(
+					tree, value, selected, expanded, leaf,row,hasFocus);
+			DefaultMutableTreeNode node;
+			
+			try {
+				// if this is a rule, check if theory is active, otherwise this
+				// is the theory itself.
+				if (leaf) node = (DefaultMutableTreeNode)
+							((DefaultMutableTreeNode)value).getParent();
+				else node = (DefaultMutableTreeNode)value;
+				String thy = (String)node.getUserObject();
+				
+				// ghost theories that aren't active
+				if (activeTheories.contains(thy) || thy=="Theories")
+					cell.setForeground(Color.black);
+				else
+					cell.setForeground(Color.gray);
+			} catch (ClassCastException e) {
+				// if any of the casts fail, we've messed up in building the tree
+				throw new QuantoCore.FatalError("Unexpected object in TheoryTree of type "
+						+ e.getMessage());
+			}
+			
+			return cell;
+		}
 	}
 	
 //	public static class RuleInfo {
