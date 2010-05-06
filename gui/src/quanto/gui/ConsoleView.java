779525f191fdc58b1d2bce1546ad3ce6c9e4c2d7
randomguy3 <randomguy3@f8f375fb-4f10-0410-8d47-b6fbdc7ca053>
2010-05-06 14:19:32 +0000

    Tidy up the console view code slightly, and add an "Exec" button.
    git-svn-id: https://isaplanner.svn.sourceforge.net/svnroot/isaplanner/trunk/quantomatic@2531 f8f375fb-4f10-0410-8d47-b6fbdc7ca053

--- gui/src/quanto/gui/ConsoleView.java
+++ gui/src/quanto/gui/ConsoleView.java
@@ -3,6 +3,7 @@ package quanto.gui;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.event.KeyAdapter;
@@ -28,7 +29,7 @@ public class ConsoleView extends InteractiveView {
 	private String prompt;
 	private JTextComponent lastFocusOwner = null;
 
-	class QuantoConsoleOutputStream extends OutputStream {
+	private class QuantoConsoleOutputStream extends OutputStream {
 
 		JTextArea textArea;
 
@@ -40,6 +41,158 @@ public class ConsoleView extends InteractiveView {
 		public void write(int b) throws IOException {
 			textArea.append(String.valueOf((char) b));
 			textArea.setCaretPosition(textArea.getDocument().getLength() - 1);
+		}
+	}
+
+	private class CommandEntryListener
+		extends KeyAdapter
+		implements ActionListener
+	{
+		public void actionPerformed( final ActionEvent e )
+		{
+			submitCommand(input);
+			input.requestFocusInWindow();
+		}
+
+		@Override
+		public void keyReleased( final KeyEvent e )
+		{
+			if ( e.isConsumed() )
+			{
+				return;
+			}
+
+			JTextField tf = (JTextField) e.getSource();
+			switch (e.getKeyCode()) {
+				case KeyEvent.VK_UP:
+					if (hpointer > 0) {
+						tf.setText(history.get(--hpointer));
+					}
+					e.consume();
+					break;
+				case KeyEvent.VK_DOWN:
+					if (hpointer < history.size() - 1) {
+						tf.setText(history.get(++hpointer));
+					}
+					e.consume();
+					break;
+				case KeyEvent.VK_TAB:
+					showCompletions(tf);
+					e.consume();
+					break;
+				case KeyEvent.VK_ENTER:
+					submitCommand(tf);
+					e.consume();
+					break;
+			}
+		}
+
+		private void showCompletions(JTextField tf) {
+			SortedSet<String> compl = core.getCompleter().getCompletions(tf.getText());
+			if (compl.size() == 1) {
+				tf.setText(compl.first());
+			}
+			else if (compl.size() > 1) {
+				tf.setText(Completer.greatestCommonPrefix(compl));
+				out.println();
+				for (String c : compl) {
+					out.println(c);
+				}
+				out.print(prompt);
+			}
+		}
+
+		private void submitCommand(JTextField tf)
+		{
+			String text = tf.getText();
+			write(text);
+			tf.setText("");
+			if (history.isEmpty() || !history.peek().equals(text)) {
+				history.push(text);
+			}
+			hpointer = history.size();
+		}
+	}
+
+	private FocusListener focusListener = new FocusListener() {
+		public void focusGained(FocusEvent e) {
+			lastFocusOwner = (JTextComponent)e.getComponent();
+			updateSelectionCommands();
+		}
+
+		public void focusLost(FocusEvent e) {
+			if (!e.isTemporary()) {
+				lastFocusOwner = null;
+				updateSelectionCommands();
+			}
+		}
+	};
+	private CaretListener caretListener = new CaretListener() {
+		public void caretUpdate(CaretEvent e) {
+			if (isAttached()) {
+				updateSelectionCommands();
+			}
+		}
+	};
+
+	public ConsoleView() {
+		super("console");
+
+		this.setLayout(new BorderLayout());
+		output = new JTextArea();
+		output.setEditable(false);
+		output.addFocusListener(focusListener);
+		output.addCaretListener(caretListener);
+		out = new PrintStream(new QuantoConsoleOutputStream(output));
+
+		QuantoCore tempCore;
+		try {
+			tempCore = new QuantoCore(out);
+			prompt = tempCore.receive();
+			out.print(prompt);
+		}
+		catch (IOException e) {
+			prompt = "*** Failed to start Quanto core ***";
+			out.print(prompt);
+			tempCore = null;
+		}
+		core = tempCore;
+
+		history = new Stack<String>();
+		input = new JTextField();
+		input.setFocusTraversalKeysEnabled(false);
+		input.addFocusListener(focusListener);
+		input.addCaretListener(caretListener);
+		CommandEntryListener listener = new CommandEntryListener();
+		input.addKeyListener(listener);
+
+		JButton execButton = new JButton("Exec");
+		execButton.addActionListener(listener);
+
+		JPanel commandPane = new JPanel(new BorderLayout());
+		commandPane.add(input, BorderLayout.CENTER);
+		commandPane.add(execButton, BorderLayout.LINE_END);
+
+		JScrollPane scroll = new JScrollPane(output);
+		scroll.setPreferredSize(new Dimension(800, 600));
+		this.add(scroll, BorderLayout.CENTER);
+		this.add(commandPane, BorderLayout.PAGE_END);
+	}
+
+	public void write(String text) {
+		synchronized (core) {
+			try {
+				out.println(text);
+				core.send(text);
+				String rcv = core.receiveOrFail();
+				out.print(rcv);
+			}
+			catch (QuantoCore.ConsoleError e) {
+				out.print("ERROR: ".concat(e.getMessage()));
+			}
+
+			// print the prompt
+			out.print(core.receive());
 		}
 	}
 
@@ -70,124 +223,6 @@ public class ConsoleView extends InteractiveView {
 				vp.setCommandEnabled(ViewPort.DESELECT_ALL_ACTION,
 					true);
 			}
-		}
-	}
-
-	private FocusListener focusListener = new FocusListener() {
-		public void focusGained(FocusEvent e) {
-			lastFocusOwner = (JTextComponent)e.getComponent();
-			updateSelectionCommands();
-		}
-
-		public void focusLost(FocusEvent e) {
-			if (!e.isTemporary()) {
-				lastFocusOwner = null;
-				updateSelectionCommands();
-			}
-		}
-	};
-	private CaretListener caretListener = new CaretListener() {
-		public void caretUpdate(CaretEvent e) {
-			if (isAttached()) {
-				updateSelectionCommands();
-			}
-		}
-	};
-
-	public ConsoleView() {
-		super("console");
-
-		this.setLayout(new BorderLayout());
-		history = new Stack<String>();
-		input = new JTextField();
-		input.setFocusTraversalKeysEnabled(false);
-		input.addFocusListener(focusListener);
-		input.addCaretListener(caretListener);
-		output = new JTextArea();
-		output.setEditable(false);
-		output.addFocusListener(focusListener);
-		output.addCaretListener(caretListener);
-		out = new PrintStream(new QuantoConsoleOutputStream(output));
-
-		QuantoCore tempCore;
-		try {
-			tempCore = new QuantoCore(out);
-			prompt = tempCore.receive();
-			out.print(prompt);
-		}
-		catch (IOException e) {
-			prompt = "*** Failed to start Quanto core ***";
-			out.print(prompt);
-			tempCore = null;
-		}
-		core = tempCore;
-
-		// print and save the default prompt
-
-
-		input.addKeyListener(new KeyAdapter() {
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				JTextField tf = (JTextField) e.getSource();
-				String text;
-				SortedSet<String> compl;
-				switch (e.getKeyCode()) {
-					case KeyEvent.VK_UP:
-						if (hpointer > 0) {
-							tf.setText(history.get(--hpointer));
-						}
-						break;
-					case KeyEvent.VK_DOWN:
-						if (hpointer < history.size() - 1) {
-							tf.setText(history.get(++hpointer));
-						}
-						break;
-					case KeyEvent.VK_TAB:
-						compl = core.getCompleter().getCompletions(input.getText());
-						if (compl.size() == 1) {
-							input.setText(compl.first());
-						}
-						else if (compl.size() > 1) {
-							input.setText(Completer.greatestCommonPrefix(compl));
-							out.println();
-							for (String c : compl) {
-								out.println(c);
-							}
-							out.print(prompt);
-						}
-						break;
-					case KeyEvent.VK_ENTER:
-						text = tf.getText();
-						write(text);
-						tf.setText("");
-						history.push(text);
-						hpointer = history.size();
-						break;
-				}
-			}
-		});
-
-		JScrollPane scroll = new JScrollPane(output);
-		scroll.setPreferredSize(new Dimension(800, 600));
-		this.add(scroll, BorderLayout.CENTER);
-		this.add(input, BorderLayout.SOUTH);
-	}
-
-	public void write(String text) {
-		synchronized (core) {
-			try {
-				out.println(text);
-				core.send(text);
-				String rcv = core.receiveOrFail();
-				out.print(rcv);
-			}
-			catch (QuantoCore.ConsoleError e) {
-				out.print("ERROR: ".concat(e.getMessage()));
-			}
-
-			// print the prompt
-			out.print(core.receive());
 		}
 	}
 
