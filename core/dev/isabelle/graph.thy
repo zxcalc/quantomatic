3c2e3e0bc10a61d09d336ed97698e47210c1f108
Lucas Dixon <lucas.dixon@gmail.com>
2011-09-29 01:05:58 -0400

    added some theory files to load up Quanto on top of Isabelle.

---
+++ core/dev/isabelle/graph.thy
@@ -0,0 +1,18 @@
+theory graph
+imports Main expressions
+uses
+(* Graphs *)
+ "../../../../quantomatic/core/graph/arity.ML" (* arity of vertices (in,out,undir) *)
+ "../../../../quantomatic/core/graph/graph_param.ML"
+ "../../../../quantomatic/core/graph/basic_graph.ML" (* basic graphs, just the data *)
+ "../../../../quantomatic/core/graph/graph.ML" (* graphs with interesting functions *)
+ "../../../../quantomatic/core/graph/graph_iso.ML" (* isomorphism between graphs *)
+ "../../../../quantomatic/core/graph/overtex.ML" (* open graphs *)
+ "../../../../quantomatic/core/graph/ograph_param.ML"
+ "../../../../quantomatic/core/graph/ograph.ML"
+ "../../../../quantomatic/core/graph/bang_graph.ML" (* bang box graphs *)
+ "../../../../quantomatic/core/graph/bang_graph_iso.ML" (* isomorphism between !graphs *)
+begin
+
+end;
+
