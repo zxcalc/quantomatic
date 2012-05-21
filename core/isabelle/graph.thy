544ee74a32bff9507796100b7788387462146ef5
Gudmund Grov <gudmundgrov@googlemail.com>
2012-05-21 16:57:13 +0100

    fixed vertex and graph for isabelle

--- core/isabelle/graph.thy
+++ core/isabelle/graph.thy
@@ -12,6 +12,16 @@ uses
  "../../core/graph/ograph.ML"
  "../../core/graph/bang_graph.ML" (* bang box graphs *)
  "../../core/graph/bang_graph_iso.ML" (* isomorphism between !graphs *)
+
+(* Metrics *)
+ "../../core/metrics/metric_type.ML" (* Utils to handle int tuples *)
+ "../../core/metrics/metrics/metric.ML" (* METRIC signature *)
+ "../../core/metrics/metrics/edge_complexity_metric.ML"
+ "../../core/metrics/metrics/weighted_arity_metric.ML"
+ "../../core/metrics/metrics/sets_cardinals_metric.ML"
+ "../../core/metrics/metrics.ML" (* Metrics on graphs *)
+(*  *)
+
 begin
 
 end;
