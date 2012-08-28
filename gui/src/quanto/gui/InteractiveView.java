d20851050e7185f8cd9ad85cb13efbe716cb0092
Alex Merry <alex.merry@cs.ox.ac.uk>
2012-08-28 18:34:21 +0100

    Remove unused import

--- gui/src/quanto/gui/InteractiveView.java
+++ gui/src/quanto/gui/InteractiveView.java
@@ -3,7 +3,6 @@ package quanto.gui;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Insets;
-import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.LinkedList;
