ac842f0101c4aa3e518681adf9852027929dc91a
Aleks Kissinger <aleks0@gmail.com>
2012-10-21 22:47:48 +0100

    finished implementing help command

--- console/quanto.rb
+++ console/quanto.rb
@@ -14,12 +14,16 @@ def JSON.parse_fragment(str)
   JSON.parse("[#{str}]")[0]
 end
 
+def JSON.generate_fragment(obj)
+  JSON.generate([obj])[1..-2]
+end
+
 def output_str(s)
   $stdout.write(s.gsub(/\e/, '[ESC]'))
 end
 
 def send_request(qin, modl, fun, json)
-  req = "\e<#{$seq}\e,#{$controller}\e,#{modl}\e,#{fun}\e,#{json}\e>"
+  req = "\e<#{$seq}\e,#{$controller}\e,#{modl}\e,#{fun}\e,#{JSON.generate_fragment(json)}\e>"
   
   if $debug
     $stdout.write('>> ')
@@ -69,35 +73,45 @@ end
 
 def run_command(qin,qout,cmd)
   if cmd == 'version'
-    send_request(qin, '!!', 'version', 'null')
+    send_request(qin, '!!', 'version', nil)
     version = get_response(qout)[2]
     puts "Quantomatic v#{version}"
   elsif cmd =~ /^help ([^:]*)(::(.*))?$/
     if $3 != nil
-      send_request(qin, '!!', 'help', "{\"module\":\"#{$1}\",\"function\":#{$3}}")
-      puts "#{$1}::#{$3}"
-      puts
-      puts 
-      puts
+      send_request(qin, '!!', 'help', {:module=>$1, :function=>$3})
+      _, status, resp = get_response(qout)
+      
+      if status == 'OK'
+        puts "Function: #{$1}::#{$3}\n  #{resp}\n\n"
+      else
+        puts "ERROR: #{resp['message']}"
+      end
     else
-      puts "help for module: #{$1}"
+      send_request(qin, '!!', 'help', {:module=>$1})
+      _, status, resp = get_response(qout)
+      
+      if status == 'OK'
+        puts "\nModule: #{$1}\n  #{resp}\n\n"
+      else
+        puts "ERROR: #{resp['message']}"
+      end
     end
   end
 end
 
 Open3.popen3("../core/bin/quanto-core") do |qin, qout, qerr, wait_thr|
   
-  send_request(qin, '!!', 'version', 'null')
+  send_request(qin, '!!', 'version', nil)
   version = get_response(qout)[2]
   puts "Quantomatic v#{version}"
   
   loop do
     begin
       cmd = Readline.readline('quanto> ')
-      break if cmd == nil
+      break if cmd == nil or cmd == 'exit'
       run_command(qin,qout,cmd)
     rescue Interrupt
-      puts "interrupt"
+      puts "^C"
       break
     end
   end
