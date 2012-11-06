c195e0dbff37e500135643e61bd41132dfad87bb
Aleks Kissinger <aleks0@gmail.com>
2012-11-06 14:28:49 +0000

    got scala talking to core. also both error and success use output field. output for errors contains message and code

--- scala/build.sbt
+++ scala/build.sbt
@@ -6,4 +6,4 @@ scalaVersion := "2.9.2"
 
 resolvers += "repo.codahale.com" at "http://repo.codahale.com"
 
-libraryDependencies += "com.codahale" % "jerkson_2.9.1" % "0.5.0"
+libraryDependencies += "com.codahale" % "jerkson_2.9.1" % "0.5.0" withSources()
--- scala/src/main/scala/test.scala
+++ scala/src/main/scala/test.scala
@@ -1,5 +1,8 @@
 import quanto.core._
+import com.codahale.jerkson.Json
+import java.io.ByteArrayInputStream
 
+case class CoreResponse(val request_id: Int, val success: Boolean, val output: Any)
 
 object Test {
   def main(args: Array[String]) = {
@@ -7,19 +10,24 @@ object Test {
     println("starting core...")
     coreProc.startCore("/Users/aleks/git/quanto/quantomatic/core/bin/quanto-core")
     println("done")
-    val json = """
-    {"request_id":0,
+    val json = (id : Integer) => """
+    {"request_id":"""+id+""",
      "controller":"red_green",
      "module":"Main",
      "function":"echo",
-     "input":1337}"""
+     "input":{"foo":1337}}"""
     
-    println("writing to core...")
-    coreProc.stdin.write(json, 0, json.length) 
-    coreProc.stdin.flush()
-    println("done. reading from core...")
+    for (i <- 0 to 3) {
+	    println("writing to core...")
+	    coreProc.stdin.write(json(i), 0, json(i).length) 
+	    coreProc.stdin.flush()
+	    println("done.\nreading from core...")
+	    val resp = Json.parse[CoreResponse](coreProc.stdout)
+	    println(resp)
+	    println("done.")
+    }
     
-    //val out = Json.stream[Map[String,Any]](coreProc.stdout)
+    
     coreProc.killCore(false)
   }
 }
