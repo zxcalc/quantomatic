022f7cb1a1737df18274afd737b95a4f39894ca1
Aleks Kissinger <aleks0@gmail.com>
2012-11-06 15:43:04 +0000

    implemented crude request method in core. takes in JSON string, should take an object of some kind

--- scala/src/main/scala/quanto/core/Core.scala
+++ scala/src/main/scala/quanto/core/Core.scala
@@ -1,5 +1,24 @@
 package quanto.core
+import org.codehaus.jackson.JsonNode
+import com.codahale.jerkson.Json
 
-class Core {
-  def test() = "foo"
+case class CoreResponse(val request_id: Int, val success: Boolean, val output: JsonNode) {
+  def output[A](implicit mf: Manifest[A]): A = Json.parse[A](output)
+}
+case class CoreError(val message: String, val code: Int) extends Exception(message)
+
+class Core(executable: String) {
+  val process = new CoreProcess
+  
+  def start() { process.startCore(executable) }
+  def stop() { process.killCore(true) }
+  def kill() { process.killCore(false) }
+  
+  def request[A](s: String)(implicit mf: Manifest[A]) : A = {
+    process.stdin.write(s, 0, s.length)
+    process.stdin.flush()
+    val resp = Json.parse[CoreResponse](process.stdout)
+    if (resp.success) resp.output[A]
+    else throw resp.output[CoreError]
+  }
 }
--- scala/src/main/scala/test.scala
+++ scala/src/main/scala/test.scala
@@ -1,33 +1,49 @@
 import quanto.core._
 import com.codahale.jerkson.Json
-import java.io.ByteArrayInputStream
-
-case class CoreResponse(val request_id: Int, val success: Boolean, val output: Any)
+import org.codehaus.jackson.JsonNode
 
 object Test {
   def main(args: Array[String]) = {
-    val coreProc = new CoreProcess
+    val core = new Core("/Users/aleks/git/quanto/quantomatic/core/bin/quanto-core")
     println("starting core...")
-    coreProc.startCore("/Users/aleks/git/quanto/quantomatic/core/bin/quanto-core")
+    core.start()
     println("done")
-    val json = (id : Integer) => """
-    {"request_id":"""+id+""",
+    val jsons = List(
+    """
+    {"request_id":0,
      "controller":"red_green",
      "module":"Main",
      "function":"echo",
-     "input":{"foo":1337}}"""
+     "input":{"foo":1337}}
+    """,
+    """
+    {"request_id":1,
+     "controller":"red_green",
+     "module":"Main",
+     "function":"concat",
+     "input":{"arg1":"call me ", "arg2":"ishmael"}}
+    """,
+    """
+    {"request_id":2,
+     "controller":"red_green",
+     "module":"Main",
+     "function":"echoo",
+     "input":{"foo":1337}}
+    """)
     
-    for (i <- 0 to 3) {
-	    println("writing to core...")
-	    coreProc.stdin.write(json(i), 0, json(i).length) 
-	    coreProc.stdin.flush()
-	    println("done.\nreading from core...")
-	    val resp = Json.parse[CoreResponse](coreProc.stdout)
+    for (json <- jsons) {
+      try {
+        println("sending request to core...")
+	    val resp = core.request[JsonNode](json)
 	    println(resp)
 	    println("done.")
+      } catch {
+        case e : CoreError => println("Error: " + e.message)
+      }
     }
     
     
-    coreProc.killCore(false)
+    
+    core.kill()
   }
 }
