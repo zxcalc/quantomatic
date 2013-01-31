e0161fc86edd78daf7c37fdd7099d52c56b7c8c3
Aleks Kissinger <aleks0@gmail.com>
2013-01-31 18:16:12 +0000

    added quadtree implementation

---
+++ scala/src/main/scala/quanto/util/QuadTree.scala
@@ -0,0 +1,98 @@
+package quanto.util
+
+import math.abs
+
+/**
+ * A quadtree for 2D spacial queries
+ */
+
+sealed abstract class QuadTree[A] {
+  def x1: Double
+  def y1: Double
+  def x2: Double
+  def y2: Double
+
+  def value: Option[A]
+  def insert(p: (Double,Double), v: A): QuadTree[A]
+
+  // visit each node/leaf with 'f', recursing until f returns true or a leaf is encountered
+  def visit(f : A => Boolean)
+}
+
+case class QuadNode[A](
+  x1: Double, y1: Double,
+  x2: Double, y2: Double,
+
+  value: Option[A],
+
+  nw: QuadTree[A],
+  ne: QuadTree[A],
+  sw: QuadTree[A],
+  se: QuadTree[A]) extends QuadTree[A]
+{
+  lazy val children = Seq(nw,ne,sw,se)
+  val midX = (x2 - x1) / 2
+  val midY = (y2 - y1) / 2
+
+  def insert(p: (Double,Double), v: A) = p match {
+    case (x,y) if x <  midX && y <  midY => copy(nw = nw.insert(p,v))
+    case (x,y) if x >= midX && y <  midY => copy(ne = ne.insert(p,v))
+    case (x,y) if x <  midX && y >= midY => copy(sw = sw.insert(p,v))
+    case (x,y) if x >= midX && y >= midY => copy(se = se.insert(p,v))
+  }
+
+  def visit(f: A => Boolean) {
+    value match {
+      case None =>
+        nw.visit(f)
+        ne.visit(f)
+        sw.visit(f)
+        se.visit(f)
+      case Some(v) =>
+        if (!f(v)) {
+          nw.visit(f)
+          ne.visit(f)
+          sw.visit(f)
+          se.visit(f)
+        }
+    }
+  }
+}
+
+object QuadNode {
+
+  // a new empty quad node with given bounds
+  def apply[A](x1: Double, y1: Double, x2: Double, y2: Double, value: Option[A]): QuadNode[A] = {
+    val midX = (x2 - x1) / 2
+    val midY = (y2 - y1) / 2
+    val nw = QuadLeaf[A](x1,y1,midX,midY)
+    val ne = QuadLeaf[A](midX,y1,x2,midY)
+    val sw = QuadLeaf[A](x1,midY,midX,y2)
+    val se = QuadLeaf[A](midX,midY,x2,y2)
+    QuadNode[A](x1,y1,x2,y2,value,nw,ne,sw,se)
+  }
+}
+
+case class QuadLeaf[A](
+  x1: Double, y1: Double,
+  x2: Double, y2: Double,
+
+  p: (Double,Double) = (0,0),
+  value: Option[A] = None) extends QuadTree[A]
+{
+  def insert(p1: (Double,Double), v1: A) =
+    value match {
+      case None => copy(p = p1, value = Some(v1))
+      case Some(v) =>
+        if (abs(p._1 - p1._1) < 0.01 && abs(p._2 - p1._2) < 0.01) {
+          // if they are very close, place the current value on the node, and add the new value as a leaf
+          QuadNode[A](x1,y1,x2,y2,Some(v)).insert(p1, v1)
+        } else {
+          // otherwise place them on their own leaves
+          QuadNode[A](x1,y1,x2,y2,None).insert(p, v).insert(p1,v1)
+        }
+    }
+
+  def visit(f: A => Boolean) { value.map(f(_)) }
+}
+
