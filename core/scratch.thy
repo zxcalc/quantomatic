443a8994df2f102798992f130c2cbc4759599326
Aleks Kissinger <aleks0@gmail.com>
2016-11-05 15:28:21 +0100

    picking biggest neighbourhood to try first

--- core/scratch.thy
+++ core/scratch.thy
@@ -9,12 +9,26 @@ open RG_Theory;
 val _ = cd "/Users/alek/git/quantomatic/core/test";
 val g = load_graph "graphs/target-test";
 val r = snd (load_rule "rules/rotate-targeted");
+val l = Rule.get_lhs r;
+val vs = Graph.get_vertices_in_bbox l (B.mk "bx0");
+val bb = Graph.is_bboxed l (V.mk "v0");
 
-val SOME v = min_arity_vertex_where is_interior_green g;
-val pre = VVInj.empty |> VVInj.add (V.mk "v10", v);
-val m = MatchSearch.match_with_prematch (Rule.get_lhs r) g pre;
 
-PolyML.exception_trace (fn () => Seq.pull m);
+fun max_concrete_nhd_bbox g bset = let
+  fun concrete_nhd_size bb = 
+    V.NSet.cardinality (V.NSet.filter
+      (not o Graph.is_bboxed g)
+      (Graph.get_adj_vertices_to_set g
+        (Graph.get_vertices_in_bbox g bb)))
+  fun max bb' v = let
+    val i' = concrete_nhd_size bb'
+  in case v
+       of SOME (i,bb) => if (i' > i) then SOME (i',bb') else SOME (i,bb)
+        | NONE => SOME (i',bb')
+  end
+in case fold max bset NONE of SOME (_,bb) => SOME bb | NONE => NONE
+end
+
 
 *}
 
