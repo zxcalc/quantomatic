360e9d94fa49f022de16d9084bd43b13e140fe8a
Aleks Kissinger <aleks0@gmail.com>
2017-04-04 12:00:20 +0200

    angle matcher

--- scala/src/main/scala/quanto/rewrite/AngleExpressionMatcher.scala
+++ scala/src/main/scala/quanto/rewrite/AngleExpressionMatcher.scala
@@ -3,20 +3,29 @@ package quanto.rewrite
 import quanto.data._
 import quanto.util._
 
-class AngleExpressionMatcher(pVars : Vector[String], tVars : Vector[String], mat : RationalMatrix) {
+class AngleExpressionMatcher(val pVars : Vector[String], val tVars : Vector[String], val mat : RationalMatrix) {
   def mtch(pExpr : AngleExpression, tExpr : AngleExpression) : Option[AngleExpressionMatcher] = {
-    val r1 = pVars.map { v => pExpr.coeffs.getOrElse(v, Rational(0)) * -1 }
-    val r2 = pVars.map { v => tExpr.coeffs.getOrElse(v, Rational(0)) }
+    val r1 = pVars.map { v => pExpr.coeffs.getOrElse(v, Rational(0)) }
+    val r2 = tVars.map { v => tExpr.coeffs.getOrElse(v, Rational(0)) }
     val row = r1 ++ r2 :+ (tExpr.const - pExpr.const)
     mat.gaussUpdate(row).map { m => new AngleExpressionMatcher(pVars, tVars, m) }
   }
 
-  def toMap : Map[String, AngleExpression] = {
+  def toMap : Map[String, AngleExpression] = if (mat.numCols == 0) Map() else {
     var mp = Map[String,AngleExpression]()
-    mat.rows.foreach { row =>
+    mat.rows.foldLeft(Map[String,AngleExpression]()) { (mp, row) =>
       val p = RationalMatrix.findPivot(row)
-      var e = AngleExpression(Rational(0))
+      var coeffs = Map[String,Rational]()
+      for (i <- p+1 to mat.line-1)
+        if (row(i) != Rational(0)) coeffs = coeffs + (pVars(i) -> row(i) * -1)
+      for (i <- mat.line to row.length-2)
+        if (row(i) != Rational(0)) coeffs = coeffs + (tVars(i-mat.line) -> row(i))
+      mp + (pVars(p) -> AngleExpression(row.last, coeffs))
     }
-    mp
   }
+}
+
+object AngleExpressionMatcher {
+  def apply(pVars : Vector[String], tVars : Vector[String]) : AngleExpressionMatcher =
+    new AngleExpressionMatcher(pVars, tVars, new RationalMatrix(Vector(), pVars.length))
 }
