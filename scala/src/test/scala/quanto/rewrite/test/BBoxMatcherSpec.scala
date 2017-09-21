d681f298ea4ec0fadc6cd532740019c3d708d19f
Hector <hector.miller-bakewell@cs.ox.ac.uk>
2017-09-21 12:03:20 +0100

    Failing: Added new bbox tests
    Including two nested bbox tests that fail

--- scala/src/test/scala/quanto/rewrite/test/BBoxMatcherSpec.scala
+++ scala/src/test/scala/quanto/rewrite/test/BBoxMatcherSpec.scala
@@ -42,6 +42,32 @@ class BBoxMatcherSpec extends FlatSpec {
     assert(matches.size === 2)
   }
 
+  it should "match an empty bbox exactly once on a graph" in {
+    val g1 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |  },
+        |  "bang_boxes": {
+        |    "bb0": {"contents": []}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+    val g2 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "X"}},
+        |    "v1": {"data": {"type": "Z"}}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+
+    val matches = Matcher.findMatches(g1, g2)
+
+    assert(matches.size === 1)
+  }
+
   it should "match a graph with 1 bbox on itself" in {
     val g1 = Graph.fromJson(Json.parse(
       """
@@ -60,6 +86,130 @@ class BBoxMatcherSpec extends FlatSpec {
     // should match the empty graph, then one copy of the contents
     assert(matches.size === 2)
   }
+
+
+  it should "match all vertices, including isolated ones" in {
+    val g1 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "wire_vertices": [
+        |    "b0"
+        |  ],
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |    "e0": {"src": "v0", "tgt": "b0"}
+        |  },
+        |  "bang_boxes": {
+        |    "bb0": {"contents": ["b0"]}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+    val g2 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "Z"}},
+        |    "v1": {"data": {"type": "Z"}},
+        |    "v2": {"data": {"type": "Z"}},
+        |    "v3": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |    "e0": {"src": "v0", "tgt": "v1"},
+        |    "e1": {"src": "v0", "tgt": "v2"}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+
+    val matches = Matcher.findMatches(g1, g2)
+
+    // v3 has no neighbours
+    // v1 and v2 are connected to v0
+
+    assert(matches.size === 4)
+  }
+
+
+  it should "match all neighbourless vertices" in {
+    val g1 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |  },
+        |  "bang_boxes": {
+        |    "bb0": {"contents": ["v0"]}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+    val g2 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "Z"}},
+        |    "v1": {"data": {"type": "Z"}},
+        |    "v2": {"data": {"type": "Z"}},
+        |    "v3": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |    "e0": {"src": "v0", "tgt": "v1"},
+        |    "e1": {"src": "v0", "tgt": "v2"}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+
+    val matches = Matcher.findMatches(g1, g2)
+
+    // v3 has no neighbours
+    // the empty match
+
+    assert(matches.size === 2)
+  }
+
+  it should "match powerset of neighbourless vertices" in {
+    val g1 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |  },
+        |  "bang_boxes": {
+        |    "bb0": {"contents": ["v0"]},
+        |    "bb1": {"contents": ["v0"]}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+    val g2 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "Z"}},
+        |    "v1": {"data": {"type": "Z"}},
+        |    "v2": {"data": {"type": "Z"}},
+        |    "v3": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |    "e0": {"src": "v0", "tgt": "v1"},
+        |    "e1": {"src": "v0", "tgt": "v2"}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+
+    val matches = Matcher.findMatches(g1, g2)
+
+    // v3 has no neighbours
+    // the empty match
+
+    // Then find the powerset of the above
+
+    assert(matches.size === 4)
+  }
+
 
   it should "match a graph with a bbox on a star graph" in {
     val g1 = Graph.fromJson(Json.parse(
@@ -99,6 +249,196 @@ class BBoxMatcherSpec extends FlatSpec {
     // without symmetry smashing = 6
     assert(matches.size === 1)
   }
+
+  it should "match the center of a star graph using two boxes" in {
+    val g1 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "wire_vertices": [
+        |    "b0"
+        |  ],
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "X"}},
+        |    "v1": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |    "e0": {"src": "v0", "tgt": "b0"},
+        |    "e1": {"src": "v0", "tgt": "v1"}
+        |  },
+        |  "bang_boxes": {
+        |    "bb0": {"contents": ["b0"]},
+        |    "bb1": {"contents": ["v1"]}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+    val g2 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices": {
+        |    "v0": {"data": {"type": "X"}},
+        |    "v1": {"data": {"type": "Z"}},
+        |    "v2": {"data": {"type": "Z"}},
+        |    "v3": {"data": {"type": "Z"}}
+        |  },
+        |  "undir_edges": {
+        |    "e0": {"src": "v0", "tgt": "v1"},
+        |    "e1": {"src": "v0", "tgt": "v2"},
+        |    "e2": {"src": "v0", "tgt": "v3"}
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+
+    val matches = Matcher.findMatches(g1, g2)
+
+    // effectively match on all choices from three things; including 0 matches from one box and 3 from the other!
+    assert(matches.size === 8)
+  }
+
+
+  it should "match a star of stars" in {
+    val g1 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "bang_boxes" : {
+        |    "bx0" : {
+        |      "contents" : [ "v1", "v2" ]
+        |    },
+        |    "bx1" : {
+        |      "contents" : [ "v2" ]
+        |    }
+        |  },
+        |  "node_vertices" : {
+        |    "v0" : {
+        |      "data" : {
+        |        "type" : "X",
+        |        "value" : ""
+        |      },
+        |      "annotation" : {
+        |        "coord" : [ -5.0, 2.0 ]
+        |      }
+        |    },
+        |    "v1" : {
+        |      "annotation" : {
+        |        "coord" : [ -5.0, 5.25 ]
+        |      }
+        |    },
+        |    "v2" : {
+        |      "data" : {
+        |        "type" : "X",
+        |        "value" : ""
+        |      },
+        |      "annotation" : {
+        |        "coord" : [ -5.0, 7.0 ]
+        |      }
+        |    }
+        |  },
+        |  "undir_edges" : {
+        |    "e0" : {
+        |      "src" : "v2",
+        |      "tgt" : "v1"
+        |    },
+        |    "e1" : {
+        |      "src" : "v1",
+        |      "tgt" : "v0"
+        |    }
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+    val g2 = Graph.fromJson(Json.parse(
+      """
+        |{
+        |  "node_vertices" : {
+        |    "v7" : {
+        |      "annotation" : {
+        |        "coord" : [ -9.0, 1.0 ]
+        |      }
+        |    },
+        |    "v2" : {
+        |      "data" : {
+        |        "type" : "X",
+        |        "value" : ""
+        |      },
+        |      "annotation" : {
+        |        "coord" : [ 4.0, -1.0 ]
+        |      }
+        |    },
+        |    "v6" : {
+        |      "annotation" : {
+        |        "coord" : [ -3.0, 7.0 ]
+        |      }
+        |    },
+        |    "v5" : {
+        |      "annotation" : {
+        |        "coord" : [ 3.0, 1.0 ]
+        |      }
+        |    },
+        |    "v1" : {
+        |      "data" : {
+        |        "type" : "X",
+        |        "value" : ""
+        |      },
+        |      "annotation" : {
+        |        "coord" : [ 4.0, 2.0 ]
+        |      }
+        |    },
+        |    "v0" : {
+        |      "data" : {
+        |        "type" : "X",
+        |        "value" : ""
+        |      },
+        |      "annotation" : {
+        |        "coord" : [ -3.0, 1.0 ]
+        |      }
+        |    },
+        |    "v3" : {
+        |      "data" : {
+        |        "type" : "X",
+        |        "value" : ""
+        |      },
+        |      "annotation" : {
+        |        "coord" : [ -3.0, 8.0 ]
+        |      }
+        |    }
+        |  },
+        |  "undir_edges" : {
+        |    "e1" : {
+        |      "src" : "v6",
+        |      "tgt" : "v0"
+        |    },
+        |    "e5" : {
+        |      "src" : "v0",
+        |      "tgt" : "v7"
+        |    },
+        |    "e0" : {
+        |      "src" : "v3",
+        |      "tgt" : "v6"
+        |    },
+        |    "e4" : {
+        |      "src" : "v5",
+        |      "tgt" : "v2"
+        |    },
+        |    "e3" : {
+        |      "src" : "v5",
+        |      "tgt" : "v1"
+        |    },
+        |    "e2" : {
+        |      "src" : "v0",
+        |      "tgt" : "v5"
+        |    }
+        |  }
+        |}
+      """.stripMargin), thy = rg)
+
+    val matches = Matcher.findMatches(g1, g2)
+
+    // v0 is the center
+    // v5, v6 and v7 are next layer out
+    // they connect to a further 0, 1 and 2 more nodes respectively
+
+    assert(matches.size === 1)
+  }
+
+
 
   it should "match an instance of the spider law" in {
     val g2 = Graph.fromJson(Json.parse(
