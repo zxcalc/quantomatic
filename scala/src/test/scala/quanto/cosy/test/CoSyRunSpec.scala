effc5da9720694d2c791418beda01965ae7f9037
Hector <hector.miller-bakewell@cs.ox.ac.uk>
2018-04-04 09:37:36 +0100

    Remove ignored test

--- scala/src/test/scala/quanto/cosy/test/CoSyRunSpec.scala
+++ scala/src/test/scala/quanto/cosy/test/CoSyRunSpec.scala
@@ -5,6 +5,11 @@ import quanto.cosy.CoSyRuns.CoSyZX
 import quanto.cosy._
 
 import scala.concurrent.duration.Duration
+import java.io.File
+
+import quanto.data.{Graph, Project, Theory, WireV}
+import quanto.rewrite.Matcher
+import quanto.util.FileHelper
 
 /**
   * Created by hector on 24/05/17.
@@ -13,10 +18,12 @@ class CoSyRunSpec extends FlatSpec {
 
   behavior of "ZX"
 
-  ignore should "do a small run" in {
-    val run: CoSyZX = new CoSyRuns.CoSyZX(List(), Duration.Inf, "~/cosytest", 4, 2, 2)
-
-    run.begin()
+  it should "do a small run" in {
+    // Don't test this here
+    // It isn't a standard feature
+    // And pollutes the filesystem
+    // Ask hmillerbakewell@gmail.com for more information
+    assert(1 == 1)
   }
 
 }
