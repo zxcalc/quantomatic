65374c16ab5aee7f2380efba827ef31c70868eeb
Hector <hector.miller-bakewell@cs.ox.ac.uk>
2018-04-24 17:26:18 +0100

    Autoformat: Layout
    Added explicit type values and un-shortened some variable names for clarity
    e.g. verts -> vertices

--- scala/src/main/scala/quanto/layout/DeriveLayout.scala
+++ scala/src/main/scala/quanto/layout/DeriveLayout.scala
@@ -5,21 +5,21 @@ import quanto.layout.constraint._
 
 
 class DeriveLayout {
-  def layout(derivation: Derivation) = {
+  def layout(derivation: Derivation): Derivation = {
     var steps = Map[DSName, DStep]()
 
     val layoutProc = new ForceLayout with Clusters
     layoutProc.alpha0 = 0.05
     layoutProc.keepCentered = false
 
-    while (steps.size < derivation.steps.size) derivation.steps.foreach { case (sname, step) =>
+    while (steps.size < derivation.steps.size) derivation.steps.foreach { case (stepName, step) =>
       // try to pull a parent that has already been processed, or root if step has no parent
-      val parentOpt = derivation.parentMap.get(sname) match {
+      val parentOpt = derivation.parentMap.get(stepName) match {
         case Some(p) => steps.get(p).map(_.graph)
         case None => Some(derivation.root)
       }
 
-      parentOpt.map { parent: Graph =>
+      parentOpt.foreach { parent: Graph =>
         var g = step.graph
         layoutProc.clearLockedVertices()
 
@@ -27,7 +27,9 @@ class DeriveLayout {
         //  are fresh for parent)
         for (v <- parent.verts) {
           if (g.verts.contains(v)) {
-            g = g.updateVData(v) { _.withCoord(parent.vdata(v).coord) }
+            g = g.updateVData(v) {
+              _.withCoord(parent.vdata(v).coord)
+            }
             layoutProc.lockVertex(v)
           }
         }
@@ -35,11 +37,11 @@ class DeriveLayout {
         layoutProc.initialize(g, randomCoords = false)
 
         // relax a bit to layout new coords
-        for (i <- 1 to 10) layoutProc.step()
+        for (_ <- 1 to 10) layoutProc.step()
         layoutProc.updateGraph()
 
         // layout the graph, and add the step
-        steps += sname -> step.copy(graph = layoutProc.graph)
+        steps += stepName -> step.copy(graph = layoutProc.graph)
       }
     }
 
--- scala/src/main/scala/quanto/layout/GraphTransform.scala
+++ scala/src/main/scala/quanto/layout/GraphTransform.scala
@@ -1,28 +1,27 @@
 package quanto.layout
 
-import quanto.data.Graph
-import quanto.data.VData
+import quanto.data.{Graph, VData}
+
+import scala.collection.mutable.ListBuffer
 // transfer each bbox to a node
 
-class GraphTransform (g:Graph){
-  
-	//val numOfBBox = g.bboxes.size
-	val bs = collection.mutable.ListBuffer[() => Iterator[VData]]()
-	
-	def transform {
-		g.bboxes.foreach(bb => 
-		  for(ver <- g.contents(bb)){
-			  val target = g.succVerts(ver)
-			  
-			  
-		  
-		})
-		
-		
-		
-	}
+class GraphTransform(g: Graph) {
 
-	def restore{
+  //val numOfBBox = g.bboxes.size
+  val bs: ListBuffer[() => Iterator[VData]] = collection.mutable.ListBuffer[() => Iterator[VData]]()
+
+  def transform() {
+    g.bboxes.foreach(bb =>
+      for (ver <- g.contents(bb)) {
+        val target = g.succVerts(ver)
+
+
+      })
 		
-	}
+
+  }
+
+  def restore() {
+
+  }
 }
